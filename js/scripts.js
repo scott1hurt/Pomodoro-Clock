38c1b54f9e4b3601f726d4ca038b26d892ca06da
Scott Hurt <scotthurt@boe_17748.barren.ketsds.net>
2019-10-29 09:55:49 -0500

    Added styling to main.scss.

--- js/scripts.js
+++ js/scripts.js
@@ -72,6 +72,19 @@ function Clock(){
     //Function to display time remaining on timer
     this.displayCurrentTime = function() {
         $('.main-display').text(formatTime(currentTime));
+        //Update the class for the progress radial to be either break or session depending on what mode we're in
+        if(mode === "Session" && $('.progress-radial').hasClass('break')) {
+            $('.progress-radial').removeClass('break').addClass('session');
+        } else if (mode === "Break" && $('.progress-radial').hasClass('session')) {
+            $('.progress.radial').removeClass('session').addClass('break');
+        }
+        //Set up the step class for the radial 
+        $('.progress-radial').attr('class', function(index, currentValue) {
+            return currentValue.replace(/(^|\s)step-\S+/g, " step-" + (100 - parseInt
+                ((currentTime / startTime) * 100)));
+
+        });console.log ($('.progress-radial').attr('class'));
+        
     }
 
     //Function to display session time 
